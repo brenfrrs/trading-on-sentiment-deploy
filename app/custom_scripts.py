6e733ebb8c0e3de32695e356a4f217d698f6989f
Brendan Ferris <brendanfrrs@gmail.com>
2021-01-03 20:11:24 -0500

    added main app folder

---
+++ app/custom_scripts.py
@@ -0,0 +1,213 @@
+import re
+import tldextract
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk.tokenize import regexp_tokenize
+import nltk
+import time
+from dateutil.relativedelta import relativedelta
+from nltk.corpus import stopwords
+
+def clean_text(text):
+    text.replace("\n"," ")
+    text =  ' '.join(re.sub("([^A-Za-z])"," ",text).split())
+    return text.lower()
+
+
+def get_outlet(link):
+    '''
+    When a URL is passed, this function will return the company name of the entity associated with the URL.
+    '''
+    res = tldextract.extract(link)
+    return res.domain
+
+def get_month_day_range(year):
+    '''
+    When given a year, returns a list of tuples with the start and end dates for each month of the provided year.
+    '''
+
+    ranges = []
+
+    target_year = [(pd.datetime(year,1,1)), (pd.datetime(year,2,1)), (pd.datetime(year,3,1)),
+           (pd.datetime(year,4,1)), (pd.datetime(year,5,1)), (pd.datetime(year,6,1)),
+           (pd.datetime(year,7,1)), (pd.datetime(year,8,1)), (pd.datetime(year,9,1)),
+           (pd.datetime(year,10,1)), (pd.datetime(year,11,1)), (pd.datetime(year,12,1))]
+
+    for date in target_year:
+        last_day = date + relativedelta(day=1, months=+1, days=-1)
+        first_day = date + relativedelta(day=1)
+        ranges.append((first_day.strftime('%Y-%m-%d'), last_day.strftime('%Y-%m-%d')))
+
+    return ranges
+
+
+def get_past_prices(range, ticker):
+    '''
+    Input: a list of date tuples in the format (%Y%m%d,%Y%m%d), and the stock ticker symbol.
+
+    Output: a dataframe with the open/close/high/low and volume historical figures related to the provided stock ticker.
+
+    Example: get_past_prices([('2010-12-01', '2010-12-31')], 'AAPL')
+    '''
+
+    td = TDClient(apikey="ef26202dacaf412fb157a05403f81ca3")
+    times = []
+
+    counter =1
+
+    for start,end in range:
+        #delay to prevent API limits.
+        time.sleep(20)
+        ts = td.time_series(
+        symbol=ticker,
+        interval="1day",
+        start_date=start,
+        end_date=end
+        ).as_pandas()
+        times.append(ts)
+        print(str(counter) + ')', str(start) + ' to',end)
+        counter +=1
+
+    stock_prices = pd.concat(times)
+
+    print("Final shape: {}".format(stock_prices.shape))
+    return stock_prices
+
+
+
+analyzer = SentimentIntensityAnalyzer()
+
+def sentiment_analyzer_scores(article):
+    '''
+    VADER Sentiment used to tag the.
+    Returns the predicted labels: positive/negative/neutral.
+    Instantiate analyzer before running this function:
+    analyzer = SentimentIntensityAnalyzer()
+    '''
+    global analyzer
+
+    score = analyzer.polarity_scores(article)
+
+    if score['compound'] >= .05:
+        sent = 'positive'
+    elif score['compound'] <= -.05:
+        sent = 'negative'
+    else:
+        sent = 'neutral'
+
+    return sent
+
+def toke(text):
+    '''
+    Input: a string value
+
+    Output: A list of tokens extracted from the string.
+    '''
+    tokens = regexp_tokenize(text, "[\w']+")
+    return tokens
+
+
+
+def unlist(x):
+    return ", ".join(x)
+
+
+tokenizer = nltk.tokenize.TweetTokenizer()
+
+def tokenize(text):
+    return nltk.word_tokenize(text)
+
+lemmatizer = nltk.stem.WordNetLemmatizer()
+
+def lemmatize_text(text):
+    '''
+    Input: a string value
+
+    Output: The lemmatized words of the passed string.
+    '''
+    return [lemmatizer.lemmatize(word) for word in text]
+
+
+
+#instantiate stop word list
+stop_words=list(set(stopwords.words("english")))
+
+#add additional stop words discovered through manually browsing the corpus.
+eda_stopwords = [
+    'x', 'u', "'", 'e', 'a', 'i', 'n', 'u', 'd', 'c', 'p', 's', 'i',
+    'o', 'r', 't', 'journalism', 'support', 'u', 'editor', 'fair', 'informed',
+    'cookie', 'miamiaccording', 'article', 'expired', 'no', 'longer', 'want',
+    'search', 'google', 'every', 'term', 'newswire', 'subscribe', 'button', 'close',
+    'accept', 'goal', 'achieve', 'u', 'subscribed', 'many', 'continue', 'offer',
+    'hard', 'provide', 'dear', 'reader', 'standard', 'always', 'strived', 'miamiinterested',
+    'adopting', 'pet', 'gazing', 'lovable', 'pup', 'adoption', 'dog', 'animal', 'shelter',
+    'ziprecruiter', 'miami', 'policy', 'clicking', 'explicit', 'consent',
+    'please', 'see', 'even', 'better', 'relevant', 'goal', 'le', 'u,', 'philip', 'schiller',
+    'believe', 'getty', 'josh', 'edelson', 'topical', 'issue', 'relevance',
+    'seen', 'man', 'forward', 'dunkin', 'late', 'wife', 'bagelsee', 'rental', 'site', 'zumper',
+    'quarantinefind', 'irvine', 'using', 'yelp', 'find', 'devon', 'horse', 'show',
+    'urge', 'turn', 'ad', 'blocker', 'telegraph', 'barbecue', 'stop', 'crunched',
+    'porch', 'ebay', 'amazon', 'curry', 'weeknightsset', 'easy', 'dinner', 'matter', 'partner',
+    'find', 'detailed', 'description', 'apartment', 'got', 'news', 'mission', 'day', 'impersonal',
+    'get', 'tip', 'top', 'mirror', 'newsletter', 'sign', 'thank', 'subscribing',
+    'newsletter', 'invalid', 'full', 'swing', 'keen', 'get', 'hand', 'high', 'street',
+    'john', 'lewis', 'curry', 'ton', 'currently', 'available', 'actual', 'check', 'back', 'also', 'honor',
+    'writer', 'try', 'put', 'apartment', 'rent', 'via', 'go', 'rounded', 'dog', 'shelter', 'pup',
+    'dozen', 'donut', 'south', 'targeted', 'practise', 'floridado', 'love', 'florida', 'doggy',
+    'cancer', 'hide', 'caption', 'cooky', 'browser', 'sauce', 'pandemicthe',
+    'something', 'penguina', 'eagle', 'email', 'notification', 'irvinein', 'hoodline',
+    'recipe', 'perfect', 'meal', 'googlethe', 'v', 'doggy', 'delightful',
+    'place', 'live', 'retire', 'takeout', 'youtubethe', 'barnes', 'museum',
+    'cooking', 'nonstick', 'cookware', 'pretzelslearn', 'homemade', 'soft',
+    'collectionsmany', 'franklin', 'u', 'gotten', 'tour', 'familiesthis',
+    'best', 'spot', 'noticed', 'adblocking', 'help', 'fund', 'award', 'winning',
+    'image', 'curry', 'ton', 'miamimiami', 'new', 'jersey', 'photographer',
+    'authoritative', 'apartment', 'cheapest', 'downtown', 'bedroom', 'adventure',
+    'aquarium', 'artwork', 'pretzel', 'click', 'play', 'tap', 'play',
+    'aught', 'newsletter', 'pear', 'david', 'nield', 'gizmodo', 'pic', 'twitter',
+    'com', 'thimbleweed', 'monument', 'pas', 'afp', 'u', 'prepear'
+]
+
+
+#extend the original stop word list to include eda stopwords.
+stop_words.extend(eda_stopwords)
+
+
+def remove_stopwords(text):
+    '''input a string and output that string with the stop words removed'''
+    return [word for word in text if word not in stop_words]
+
+
+
+
+#classification target threshold can be changed easily if needed.
+def thresh(change):
+    targ = None
+    if change >= .055: #only predict if price increase is at or above .20 cents
+        targ = 1
+    else:
+        targ = 0
+    return targ
+
+
+def process_frame(df):
+    """
+    Manages the Datetime information for the results(main) dataframe.
+    """
+    #make date index
+    df.set_index('date', inplace=True)
+
+    #Change to Datetime
+    df.index = pd.to_datetime(df.index).tz_convert('utc')
+
+    #convert DateTime index to eastern time.
+    df.index = df.index.tz_convert('US/Eastern')
+    #put into year/month/day format
+    df.index = df.index.strftime('%Y-%m-%d')
+
+    #rename to date
+    df.index.names = ["date"]
+
+    df.dropna(inplace=True)
+    df.fulltext = df.fulltext.apply(clean_text)
+    return df
---
+++ app/logic.py
@@ -0,0 +1,380 @@
+#!/bin/zsh
+
+from newspaper import fulltext, Article
+from datetime import date, timedelta
+import requests
+import pandas as pd
+from dateutil import parser
+import pandas as pd
+import numpy as np
+import time
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+import pytz
+from selectorlib import Extractor
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import ElementClickInterceptedException, UnexpectedAlertPresentException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from custom_scripts import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+import sys
+
+today = date.today()
+todays_date = today.strftime("%Y-%m-%d")
+
+co = ['apple']
+
+counter = 0
+while True:
+
+    if counter >= 1:
+        counter += 1
+        print('sleeping for 30 minutes before scraping again.')
+        time.sleep(1800) #sleep for 30 minutes after getting data for all companies in `co` list.
+    else:
+        counter += 1
+        pass
+
+    for comp in co:
+        class Collect:
+            '''
+            This class handles all of the scraping and data collection activities.
+
+            To instantiate: call `Collect('apple',time_period=<time_period>)`
+
+            Possible time periods are: 12 and 2 (hrs)
+            time_period=2 -- will scrape every article published in the last two hours.
+            time_period=12 -- will scrape evert article published in the last 12 hours.
+
+            By default, the time period defaults to 24 hours.
+
+            '''
+
+            def __init__(self, company, ticker='#', time_period='#tp_2'):
+                self.company = company
+                self.time_period = time_period
+                self.ticker = ticker
+
+                if time_period == 12:
+                    self.time_period = '#tp_12'
+                elif time_period == 2:
+                    self.time_period = '#tp_2'
+                elif time_period == 1:
+                    self.time_period = '#tp_1'
+                else:
+                    pass
+
+                #some logic so the user does not need to enter exact company names or know the ticker.
+                formed_query = self.company.lower()
+                stocks = {
+                "the walt disney company":"DIS",
+                "walmart inc.":"WMT",
+                "dow inc.":"DOW",
+                "salesforce, inc.":"CRM",
+                "nike, inc.":"NKE",
+                "the home depot, inc.":"HD",
+                "visa, inc.":"V",
+                "microsoft corporation":"MSFT",
+                "3m company":"MMM",
+                "cisco systems, inc.":"CSCO",
+                "coke":"KO",
+                "the coca-cola company":"KO",
+                "apple inc.":"AAPL",
+                "honeywell international inc.":"HON",
+                "j&j":"JNJ",
+                "johnson and johnson":"JNJ",
+                "the travelers companies, inc.":"TRV",
+                "the proctor and gamble company":"PG",
+                "chevron corporation":"CVX",
+                "verizon communications inc.":"VZ",
+                "caterpillar inc.":"CAT",
+                "the boeing company":"BA",
+                "amgen inc.":"AMGN",
+                "ibm":"IBM",
+                "international business machines corporation":"IBM",
+                "american express company":"AXP",
+                "jpmorgan chase & co.":"JPM",
+                "walgreens boots alliance, inc.":"WBA",
+                "mcdonalds corporation":"MCD",
+                "merck & co., inc.": "MRK",
+                "the goldman sachs group, inc.":"GS",
+                "unitedhealth group incorporated":"UNH",
+                "intel corporation":"INTC"
+                }
+
+                try:
+                    result = {key:value for (key,value) in stocks.items() if formed_query in key}
+                    self.ticker = str(list(result.values())[0])
+                except IndexError as e:
+                    raise type(e)('That is not a valid name for a stock in the DOW!')
+
+
+
+            def fetch_articles(self):
+                '''
+                Scrapes newslookup.com for a given stock and time interval.
+
+                Company name and time interval declared during class instantiation, by default it will
+                scrape all articles published in the last 24 hours.
+
+                '''
+                now = datetime.now()
+                todays_dt = now.strftime("%Y-%m-%d@%I:%M:%S%p") #used for file naming.
+                ChromeOptions = webdriver.ChromeOptions()
+                ChromeOptions.add_argument('--incognito')
+                ChromeOptions.add_argument('--headless')
+                driver = webdriver.Chrome(ChromeDriverManager().install(), options=ChromeOptions)
+                time.sleep(2)
+                print('navigating to newslookup..')
+                news_look = "https://www.newslookup.com"
+                time.sleep(1)
+                driver.get(news_look)
+                time.sleep(3)
+
+                driver.find_element_by_css_selector('#lookup').send_keys(self.company)
+
+                #search button
+                driver.find_element_by_css_selector('#form-group > div > span > button').click()
+
+                #time period
+                driver.find_element_by_css_selector('#timeperiod').click()
+
+                #past 24 hours--------------------------------------------------
+                driver.find_element_by_css_selector(self.time_period).click()
+
+                #scroll down
+                for loaded_page in range(10000):
+                    try:
+                        print(f'{loaded_page}')
+                        time.sleep(1.50)
+                        driver.find_element_by_tag_name('body').send_keys(Keys.END)
+                        time.sleep(1.50)
+                        driver.find_element_by_css_selector('#more-btn').click()
+                    except ElementNotInteractableException:
+                        print('no more pages')
+                        break
+                    except (TimeoutException, ElementClickInterceptedException) as e: #extraneous errors usually fixed after retry.
+                        continue
+                    except NoSuchElementException:
+                        break
+
+                results_list = [] #list of all the links and associated publish dates.
+
+                raw_html = driver.page_source
+                extracted_text = Extractor.from_yaml_string("""
+                card:
+                    css: 'div#results'
+                    xpath: null
+                    type: Text
+                    children:
+                        title:
+                            css: 'a.title:nth-of-type(n+4)'
+                            xpath: null
+                            multiple: true
+                            type: Link
+                        date:
+                            css: span.stime
+                            xpath: null
+                            multiple: true
+                            type: Text
+
+                 """)
+                raw_data = extracted_text.extract(raw_html)
+
+                #combines the links with their appropriate publish dates (dates in UTC format at this point.)
+                results = list(zip(raw_data['card']['title'],raw_data['card']['date'][1:]))
+                for entry in results:
+                    results_list.append(entry)
+
+                final_df = pd.DataFrame(columns= ['update', 'date', 'source', 'author', 'fulltext', 'summary'])
+
+
+                counter = 0
+                for link, date in results_list:
+                    try:
+                        article= Article(link)
+                        article.download()
+                        article.parse()
+                        text = article.text
+                        published = article.publish_date
+                        auth = article.authors
+                        source = link
+                        summ = article.summary
+                        titl = article.title
+                        final_df = final_df.append({'update':published, 'date':date, 'source':link, 'author':auth, 'fulltext':text, 'summary':summ, 'title':titl}, ignore_index=True)
+                        counter += 1
+                        final_df = final_df.drop_duplicates(subset='fulltext')
+                        print(f"{final_df.shape[0]} unique articles, {counter - final_df.shape[0]} duplicates removed.")
+                    except:
+                        continue
+                final_df = final_df.drop_duplicates(subset='fulltext')
+                final_df.drop(columns=['summary', 'author', 'update'], inplace=True)
+                final_df.dropna(subset=['fulltext'], inplace=True)
+                driver.close()
+
+                #save to results to csv (but there are duplicate entries that need removal)
+                filepath = 'daily_data/{}/full_results.csv'.format(self.company)
+                with open(filepath, 'a') as f:
+                    final_df.to_csv(f, header=False) #If ran for the first time, needs to be set to true on the first run then back to false for all subsequent loops.
+                print(f"{final_df.shape[0]} results added to {filepath}")
+
+                #remove duplicates from the file.
+                drop_repeats = pd.read_csv(filepath, index_col=0)
+                num_repeats = drop_repeats.fulltext.duplicated().sum()
+                drop_repeats.drop_duplicates(subset=['fulltext'], inplace=True)
+                drop_repeats.to_csv(filepath)
+                print(f"{final_df.shape[0] - num_repeats} new articles added.")
+
+
+                return final_df
+
+
+            def current_info(self):
+                '''
+                Grabs the current stock information from yahoo finance.
+                '''
+                import yahoo_fin.stock_info as si
+                from datetime import date
+
+                tick = self.ticker
+                table = si.get_quote_table(tick)
+                today = date.today()
+
+                open_price = table['Open']
+                current_volume = table['Volume']
+                current_high = table["Day's Range"].split('-')[1].lstrip()
+                current_low = table["Day's Range"].split('-')[0].rstrip()
+                today_date = today.strftime('%Y-%m-%d')
+
+                row_data = {'open':open_price,
+                    'volume': current_volume,
+                    'high': current_high,
+                    'low':current_low,
+                    'date':today_date}
+
+                df = pd.DataFrame.from_dict(row_data, orient='index').T
+
+                #convert date column to datetime series
+                datetime_series = pd.to_datetime(df.date)
+
+                #convert the series to a DateTime index.
+                datetime_index = pd.DatetimeIndex(datetime_series.values)
+
+                #set as the index of the df.
+                df = df.set_index(datetime_index)
+
+                #drop original date column
+                df.drop(columns=['date'], inplace=True)
+                return df
+
+        #company name provided as arguments when ran from CLI
+        company = comp.lower()
+
+        query = Collect(company)
+        results = query.fetch_articles()
+
+        def vader_sent(company_name):
+            filepath = 'daily_data/{}/full_results.csv'.format(company_name)
+            results = pd.read_csv(filepath, index_col=0)
+            results.dropna(inplace=True)
+
+            results['sentiment'] = results['fulltext'].apply(sentiment_analyzer_scores) #predict the sentiment of each article
+            sentiment_dummies = pd.get_dummies(results['sentiment'], prefix='sent') #dummy the sentiment values
+            results = pd.concat([results, sentiment_dummies], axis=1)
+            results.drop(columns='sentiment', inplace=True) #drop original categorical value
+
+            res = process_frame(results) #manage the conversions of the datetimes from UTC to Eastern. Can be found in custom_scripts.py.
+
+            res.fulltext = res.fulltext.apply(clean_text)
+            res['tokens'] = res['fulltext'].apply(toke)
+
+            pre_process = [remove_stopwords, lemmatize_text, unlist]
+
+            for action in pre_process:
+                res.tokens = res.tokens.apply(action)
+                print('Completed: {}'.format(str(action)))
+
+            return res
+
+
+        cleaned = vader_sent(company)
+
+        def aggregate(df):
+            df.reset_index(inplace=True)
+            df['date'] = pd.to_datetime(df['date'])
+
+            #aggregate all of the articles and associated sentiment into one column for the day.
+            sentiment = df.groupby('date')['sent_negative', 'sent_positive'].agg(np.sum)
+            text = df.groupby('date')['tokens'].agg(''.join)
+
+            df = pd.merge(text, sentiment, how='inner', left_index=True, right_index=True)
+
+            today = date.today()
+            todays_date = today.strftime("%Y-%m-%d")
+
+            df = pd.DataFrame(df.loc[todays_date]).T
+
+            return df
+
+        aggregated_row = aggregate(cleaned)
+
+        print(aggregated_row)
+
+        #merge todays price
+        todays_data = query.current_info()
+        aggregated_row=pd.merge(aggregated_row,todays_data, how='outer', left_index=True, right_index=True)
+        aggregated_row[['sent_negative', 'sent_positive', 'open', 'volume', 'high', 'low']] = aggregated_row[['sent_negative', 'sent_positive', 'open', 'volume', 'high', 'low']].apply(pd.to_numeric)
+
+        def tfidf(company, daily_row):
+            tfidf = joblib.load(f'pickles/{company}/tfidf')
+            selector = joblib.load(f'pickles/{company}/kbest')
+            rf_feats = daily_row.tokens.values
+
+            X = tfidf.transform(rf_feats).toarray()
+            X_2 = selector.transform(X)
+
+            tfidf_train = pd.DataFrame(X_2)
+
+            X_test = daily_row.reset_index(drop=True)
+            frames = [X_test, tfidf_train]
+            X_test = pd.concat(frames, axis=1)
+
+            X_test.drop(columns=['tokens'], inplace=True) #tokens not needed after TFIDF
+
+            return X_test
+
+
+        prediction_row = tfidf(query.company, aggregated_row)
+
+
+        model = joblib.load(f'pickles/{query.company}/gausfinalmodel')
+        final_result = model.predict(prediction_row)
+
+        final_proba = model.predict_proba(prediction_row)
+        prob_up = "{:.3%}".format(final_proba[:,1][0])
+        prob_down = "{:.3%}".format(final_proba[0][0:1][0])
+
+        prediction_row['prediction'] = int(final_result[0])
+        prediction_row['probability_up'] = prob_up
+        prediction_row['probability_down'] = prob_down
+        prediction_row['ticker'] = query.ticker
+
+        time_ = datetime.now() #underscore so we don't overwrite the time module.
+        pred_time = time_.strftime("%I:%M:%S %p")
+        prediction_row['updated'] = pred_time
+
+
+        prediction_row.to_csv(f"daily_predictions/{query.company}/{todays_date}.csv")
+        print(f'Scrape loop count: {counter}')
